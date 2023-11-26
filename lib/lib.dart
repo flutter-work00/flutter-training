73060fcc907c0ee5a7f2c8ea666b8a1a9d7b8a5b
yamada <116492865+yamadade@users.noreply.github.com>
2023-11-26 16:44:44 +0900

    main.dartファイルをlibディレクトリ直下に移動

--- lib/lib.dart
+++ lib/lib.dart
@@ -1,1 +1,2 @@
+export 'main.dart';
 export 'src/src.dart';
--- lib/src/src.dart
+++ lib/src/src.dart
@@ -1,5 +1,4 @@
 export 'feature/feature.dart';
-export 'main.dart';
 export 'main_app.dart';
 export 'ui/ui.dart';
 export 'utility/utility.dart';
