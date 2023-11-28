4b7aef1d0ea82ca5ade6ad542fd3e12568a14c7f
yamada <116492865+yamadade@users.noreply.github.com>
2023-11-28 23:31:20 +0900

    session4 all tasks done

--- lib/src/ui/launch_page.dart
+++ lib/src/ui/launch_page.dart
@@ -8,24 +8,22 @@ class LaunchPage extends StatefulWidget {
   State<LaunchPage> createState() => _LaunchPage();
 }
 
-class _LaunchPage extends State<LaunchPage> {
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {});
-  }
-
-  @override
-  Future<void> didChangeDependencies() async {
-    super.didChangeDependencies();
-    await GoRouterService.toWeatherPage(context: context);
-  }
-
+class _LaunchPage extends State<LaunchPage> with AfterLayoutMixin {
   @override
   Widget build(BuildContext context) {
     return PopScope(
       canPop: CanPopStatus.unPop.value,
       child: ColoredBox(color: context.colorTheme.launchPageBackground!),
     );
+  }
+
+  @override
+  Future<void> afterLayoutFunction() async {
+    await GoRouterService.toWeatherPage(context: context);
+  }
+
+  @override
+  Future<void> didChangeDependencies() async {
+    super.didChangeDependencies();
   }
 }
--- lib/src/utility/utility.dart
+++ lib/src/utility/utility.dart
@@ -2,4 +2,5 @@ export 'app_path.dart';
 export 'constant/constant.dart';
 export 'exceptions/exceptions.dart';
 export 'extension/extension.dart';
+export 'mixin/mixin.dart';
 export 'result.dart';
