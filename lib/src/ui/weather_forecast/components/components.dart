a9ce64f704302e91ede21bdd820521575b69de21
yamada <116492865+yamadade@users.noreply.github.com>
2023-12-08 13:21:53 +0900

    remove: [weather_forecast/components/]ディレクトリを削除

--- lib/src/ui/weather_forecast/components/components.dart
+++
@@ -1,2 +0,0 @@
-export 'forecast_result_image.dart';
-export 'temperature.dart';
--- lib/src/ui/weather_forecast/weather_forecast.dart
+++ lib/src/ui/weather_forecast/weather_forecast.dart
@@ -1,3 +1,4 @@
-export 'components/components.dart';
+export 'forecast_result_image.dart';
+export 'temperature.dart';
 export 'weather_action_button.dart';
 export 'weather_forecast_result.dart';
